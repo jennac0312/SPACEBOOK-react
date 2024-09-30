e5784b8dd4b079de663de573b88053986a5db4ae
Jenna C <jennac0312@gmail.com>
2024-09-30 10:40:24 -0400

    set up github pages

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AppContextProvider>
-      <Router>
+      <Router basename='/spacebook-react'>
         <App />
       </Router>
     </AppContextProvider>
